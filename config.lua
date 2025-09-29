dc14017faa801a2ce6063648aa97a4988b2a6d19
Elijah <128943965+elijahhhhhhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2025-09-29 10:03:59 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,14 +7,16 @@ Config.language = 'en'
 
 -- These are default keys to interact with dolu_tool commands.
 -- You can also override them in your pause menu, in the FiveM keybinds section.
+-- Problay going to re-write this to use mythic chat commands instead of keybinds
 Config.openMenuKey = 'F3'
 Config.toggleNoclipKey = 'F11'
 Config.teleportMarkerKey = 'F10'
 
 Config.favoriteVehicle = 'adder'
-Config.customVehiclePlate = '~DOLU~' -- Remove the text if you don't want a custom plate.
+Config.customVehiclePlate = '~MYTHIC~' -- Remove the text if you don't want a custom plate.
 
 -- Permission system based on ace permissions.
 -- If Config.permission is set to false, everyone will have access to the menu.
+-- Going to re-write this to use admin/owner mythic admin perms to open menu
 Config.usePermission = false
 Config.permission = { 'group.admin', 'qbcore.dev' }
