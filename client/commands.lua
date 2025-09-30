db452f17996f67fb4e3b46c786a16404d4e87024
Elijah <128943965+elijahhhhhhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2025-09-30 12:56:51 -0400

    Update commands.lua
    Still missing intended features

--- client/commands.lua
+++ client/commands.lua
@@ -1,4 +1,46 @@
--- Add mythic components here and function keybinds here
+
+AddEventHandler("Tool:Shared:DependencyUpdate", RetrieveComponents)
+function RetrieveComponents()
+	--Callbacks = exports["mythic-base"]:FetchComponent("Callbacks")
+	--Inventory = exports["mythic-base"]:FetchComponent("Inventory")
+	--Targeting = exports["mythic-base"]:FetchComponent("Targeting")
+	--Progress = exports["mythic-base"]:FetchComponent("Progress")
+	--Hud = exports["mythic-base"]:FetchComponent("Hud")
+	--Notification = exports["mythic-base"]:FetchComponent("Notification")
+	--ObjectPlacer = exports["mythic-base"]:FetchComponent("ObjectPlacer")
+	--Minigame = exports["mythic-base"]:FetchComponent("Minigame")
+	--ListMenu = exports["mythic-base"]:FetchComponent("ListMenu")
+	--PedInteraction = exports["mythic-base"]:FetchComponent("PedInteraction")
+	--Polyzone = exports["mythic-base"]:FetchComponent("Polyzone")
+    RegisterChatCommands()
+end
+
+AddEventHandler("Core:Shared:Ready", function()
+	exports["mythic-base"]:RequestDependencies("Tool", {
+        --"Callbacks",
+        --"Inventory",
+        --"Targeting",
+        --"Progress",
+        --"Hud",
+        --"Notification",
+        --"ObjectPlacer",
+		--"Minigame",
+		--"ListMenu",
+		--"PedInteraction",
+		--"Polyzone",
+	}, function(error)
+		if #error > 0 then 
+            exports["mythic-base"]:FetchComponent("Logger"):Critical("Tool", "Failed To Load All Dependencies")
+			return
+		end
+		RetrieveComponents()
+                
+	end)
+end)
+
+function RegisterChatCommands()
+    
+
 
 lib.addKeybind({
     name = 'dolu_tool:open',
